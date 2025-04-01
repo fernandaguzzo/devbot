35163f733b055a38f655cf2e41680b59e39c878a
fernandaguzzo <guzzofernanda84@gmail.com>
2025-04-01 20:00:30 -0300

    modificações

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -1,37 +1,37 @@
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-class ActionRecomendarFilme(Action):
+class ActionSolucionarProblema(Action):
     def name(self) -> Text:
-        return "action_recomendar_filme"
-    def run (self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-             
-             genero = tracker.get_slot("genero")
-             filmes = self.obter_recomendacoes(genero)
+        return "action_solucionar_problema"
 
-             if filmes:
-                   dispatcher.utter_message(text=f"Para o gênero {genero}, recomendo: {', '.join(filmes)}")
-             else:
-                   dispatcher.utter_message(text="Desculpe, não encontrei recomendações para esse gênero.")
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-             return []
-    
-    def obter_recomendacoes(self, genero: Text) -> List[Text]:
-          filmes_por_genero = {
-                "comedia": ["Se Beber, Não Case!", "Minha Mãe é Uma Peça", "Deadpool"],
-                "ação": ["Mad Max: Estrada da Fúria", "John Wick", "Missão Impossível: Efeito Fallout"],
-                "drama": ["Um Sonho de Liberdade", "O Poderoso Chefão", "A Lista de Schindler"],
-                "suspense": ["Psicose", "O Silêncio dos Inocenter", "Seven - Os Sete Crimes Capitais"],
-                "terror": ["O Iluminado", "Invocação do Mal", "Hereditário"],
-                "romance": ["Um Lugar Chamado Notting Hill", "Orgulho e Preconceito", "A Culpa é das Estrelas"],
-                "ficção científica": ["Interestelar", "Blade Runner 2049", "A Chegada"],
-                "documentário": ["A 13ª Emenda", "Planeta Terra", "Free Solo"],
-                "animação": ["Toy Story", "Soul", "Your Name."],
-                "faroeste": ["Era Uma Vez no Oeste", "Os Imperdoáveis", "Bravura Indômita"]
-          }
-          
-          return filmes_por_genero.get(genero.lower(), [])
+        problema = tracker.get_slot("problema")
+        
+        
+        resposta = self.resolver_problema(problema)
+
+        
+        dispatcher.utter_message(text=resposta)
+
+        return []
+
+    def resolver_problema(self, problema: Text) -> Text:
+        
+        problemas_comuns = {
+
+            "erro ao procurar música": "Reinicie o aplicativo e verificar se sua conexão com a internet.",
+            "erro no plano": "Verifique se você está logado corretamente na sua conta e se o pagamento do seu plano foi feito.",
+            "não consigo acessar a conta": "Recarregue a página e limpe o cache.",
+            "problema com assinatura": "Verifique se o seu plano de assinatura foi renovado.",
+            "erro ao carregar a página": "Recarregue a página e limpe o cache."
+
+        }
+
+        
+        return problemas_comuns.get(problema.lower(), "Não consegui encontrar uma solução para o seu problema, vou encaminhá-lo para um atendente.")
+
